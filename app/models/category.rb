2f5a8546a80bc2bac8db4824697ff980a629a4a1
jose <j.ramos.olivares@gmail.com>
2021-04-03 09:28:39 -0300

    self category relation added to model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :category, optional: true
+  belongs_to :parent_category, class_name: "Category", optional: true, foreign_key: 'category_id'
+  has_many :children_categories, class_name: "Category", foreign_key: "category_id"
 end
